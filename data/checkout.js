86b0183c94296bb235640a46b7fa0e65b42f44c7
Whitepeel <madhavvs276@gmail.com>
2024-08-10 10:58:00 +0530

    Included Backend Practice

--- data/checkout.js
+++ data/checkout.js
@@ -1,7 +1,7 @@
 import { render_order_summary } from "./checkout/order_summary.js";
 import { render_Payment_Summary } from "./checkout/payment_summary.js";
 import { quantity_update } from "./cart.js";
-import '../data/cart_class.js';
+import '../data/practice-backend.js';
 render_order_summary();
 render_Payment_Summary();
 export function checkout_quantity()
--- data/products.js
+++ data/products.js
@@ -11,6 +11,7 @@ export function get_item(product_id)
   })
   return matched;
 }
+
 class Product
 {
   id;
